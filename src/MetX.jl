583125dac70f5d6c167b71a6266c735b3fc797e9
josePereiro <jose.pereiro.personal@gmail.com>
2023-03-14 18:14:45 -0400

    WIP - Manifest

--- src/MetX.jl
+++ src/MetX.jl
@@ -1,14 +1,26 @@
 module MetX
 
-    using Reexport
+    # using Reexport
 
-    @reexport using MetXBase
-    @reexport using MetXGEMs
-    @reexport using MetXOptim
-    @reexport using MetXEP
-    @reexport using MetXMC
-    @reexport using MetXGrids
-    @reexport using MetXNetHub
-    @reexport using MetXCultureHub
+    # @reexport using MetXBase
+    # @reexport using MetXGEMs
+    # @reexport using MetXOptim
+    # @reexport using MetXEP
+    # @reexport using MetXMC
+    # @reexport using MetXGrids
+    # @reexport using MetXNetHub
+    # @reexport using MetXCultureHub
+    
+    using MetXCultureHub
+    using MetXBase
+    using MetXNetHub
+    using MetXGEMs
+    using MetXGrids
+    using MetXOptim
+    using MetXMC
+    using MetXEP
+
+    # export
+    @_exportall_non_underscore()
 
 end
